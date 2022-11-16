da518ee571fa31502e460b219c28902f5db6b5c4
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2022-11-16 19:02:24 +0000

    First upload

---
+++ Linkedinify.js
@@ -0,0 +1,20 @@
+// import findAndReplaceDOMText from 'findandreplacedomtext';
+// https://github.com/padolsey/findAndReplaceDOMText
+
+const reggie = new RegExp(/\!|\.|\?/, 'g');
+
+function makeDeeper({text}) {
+    const abbr = document.createElement('abbr');
+    abbr.title = text;
+    abbr.innerHTML = text + "<br><br>";
+    return abbr;    
+}
+
+function Linkedinify() 
+{   
+    findAndReplaceDOMText(document.body, {
+        find: reggie,
+        replace: makeDeeper,
+        preset: 'prose'
+    });
+}
